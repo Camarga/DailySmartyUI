3728e532a7ddb510c6553f958d8ec8add9cbfad0
Camarga <77camarga@gamil.com>
2025-09-30 23:27:38 +0200

    setup app routes with react-router-dom

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,24 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/app";
-import reducers from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import "./style/main.scss";
+import 'bootstrap/dist/css/bootstrap.css';
+import './style/main.scss';
+
+import Home from './components/home';
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
+        <Switch>
+          {/* Ruta para la Home */}
+          <Route exact path='/' component={Home} />
+          
+          {/* Ruta temporal para Results (todavía sin componente propio) */}
+          <Route path='/results' component={Home} />
+        </Switch>
       </BrowserRouter>
     </Provider>,
-    document.querySelector(".app-wrapper")
+    document.querySelector('.app-wrapper')
   );
 }
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
