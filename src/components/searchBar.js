cfb45368f3ad172f2484d521235993dad87e799a
Camarga <77camarga@gamil.com>
2025-10-02 12:36:07 +0200

    added react-animate-height npm package and used it to add hover animations to the post links

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -4,30 +4,29 @@ import { Field, reduxForm } from 'redux-form';
 import { withRouter } from 'react-router-dom';
 
 class SearchBar extends Component {
-  handleFormSubmit = ({ query }) => {
-    console.log('trying to handle submit for query', query);
-    this.props.history.push("/results");
+
+  handleFormSubmit = function({query}) {
+     this.props.onSubmit(query);
   }
 
-  renderInput = (field) => {
-    return <input type="text" placeholder="Search DailySmarty" {...field.input} />;
+  renderInput(field) {
+        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
   }
 
   render() {
-    const { handleSubmit } = this.props; 
-    return (
-      <form
-        className="search-bar"
-        onSubmit={handleSubmit(this.handleFormSubmit)}
-      >
-        <Field name="query" component={this.renderInput} />
-      </form>
-    );
-  }
+
+        const { handleSubmit } = this.props;
+
+        return (
+            <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <Field name="query" component={this.renderInput}/>
+            </form>
+        )
+    }
 }
 
 SearchBar = reduxForm({
-  form: 'searchBar'
+    form: 'searchBar'
 })(SearchBar);
 
 SearchBar = withRouter(SearchBar);
